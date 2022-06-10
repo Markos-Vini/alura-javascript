5ca353d5b933ed1168bc55e5b8412c679d5f5cc8
Markos-Vini <marcosevaristo91@outlook.com>
2022-06-10 13:23:36 -0300

    mudando o nome do parametro

--- funçoes/arrowfunction.js
+++ funçoes/arrowfunction.js
@@ -8,10 +8,10 @@ const soma = (num1, num2) => num1 + num2
 
 // Arrow function com mais de uma linha de instrução
 
-const somaNumerosPequenos = (num1, num2) => {
-    if (num1 || num2 >= 10){
+const somaNumerosPequenos = (numero1, numero2) => {
+    if (numero1 || numero2 >= 10){
         return 'somente numeros de 1 a 9'
     } else {
-        return num1 + num2
+        return numero1 + numero2
     }
 }
