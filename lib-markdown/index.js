84e1b4ec006641916a8501326e05a001f12f228e
Markos-Vini <marcosevaristo91@outlook.com>
2022-06-26 12:26:05 -0300

    Criando uma Função para tratamento de erros

--- lib-markdown/index.js
+++ lib-markdown/index.js
@@ -1,10 +1,16 @@
 import chalk from 'chalk'
 import fs from 'fs'
 
+function tratarErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'))
+}
 
 function pegandoArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (_,texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro,texto) => {
+        if (erro) {
+            tratarErro(erro)
+        }
         console.log(chalk.green(texto))
     })
 }
